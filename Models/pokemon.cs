5685cd246100bc51096efe60a3a37b2ffa883f61
Nafyad <nafyadhailu99@gmail.com>
2025-01-10 15:01:30 +0300

    add Abilty attribute

--- Models/pokemon.cs
+++ Models/pokemon.cs
@@ -3,15 +3,21 @@ using MongoDB.Bson.Serialization.Attributes;
 
 public class Pokemon
 {
-    // [BsonId]
-    // [BsonRepresentation(BsonType.ObjectId)]
+    
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
 
-    [BsonElement("name")]
+    [BsonElement("Name")]
     public string Name { get; set; } = null!;
 
-    [BsonElement("type")]
+    [BsonElement("Type")]
     public string Type { get; set; } = null!;
-    [BsonElement("level")]
+    [BsonElement("Level")]
     public int Level { get; set; }
+    
+    [BsonElement("Ability")]
+
+    public string Ability { get; set; } = null!;
+    
 }
